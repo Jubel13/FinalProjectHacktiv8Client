47c385962083e89c8b315f4d31bce804a8f388e5
Jubel13 <jubelsinaga13@gmail.com>
2022-03-22 13:38:46 +0700

    Integrate client

--- client/src/pages/CarList.jsx
+++ client/src/pages/CarList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Card from "../components/CarList/templates/Card";
-import { carsApi } from "../apis";
+import { carsApi } from "../API";
 
 export default function CarList() {
   const [cars, setCars] = useState([]);
@@ -8,18 +8,18 @@ export default function CarList() {
   const fetchCars = async () => {
     const response = await carsApi.get();
     // console.log(response.data)
-    setCars(response.data)
-  }
+    setCars(response.data);
+  };
   useEffect(() => {
-    fetchCars()
+    fetchCars();
   }, []);
 
   return (
-    <div className="h-full w-full mt-8 px-36 flex flex-col gap-y-12">
+    <div className='h-full w-full mt-8 px-36 flex flex-col gap-y-12'>
       {/* <div className="h-24 w-full z-50 bg-slate-600 flex flex-col justify-center items-center sticky top-0">
         Search
       </div> */}
-      <div className="h-full w-full flex flex-wrap gap-y-8 items-center justify-center">
+      <div className='h-full w-full flex flex-wrap gap-y-8 items-center justify-center'>
         {cars.map((car, index) => {
           return <Card key={index} car={car} />;
         })}

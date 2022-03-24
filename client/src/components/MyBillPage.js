7b476f216bb486cf65718f7617abe25e9d70e8dc
fidairvananas <fidairvan5@gmail.com>
2022-03-24 12:39:29 +0700

    update my bill

--- client/src/components/MyBillPage.js
+++ client/src/components/MyBillPage.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import serverApi from '../API/serverApi';
-import {  TailSpin } from 'react-loader-spinner';
+import Swal from 'sweetalert2';
 
 export default function MyBill() {
   const [boughtHistory, setBoughtHistory] = useState([]);
+  const [boughtHistoryCash, setBoughtHistoryCash] = useState({})
+  const [boughtHistoryCredit, setBoughtHistoryCredit] = useState([])
+  const [showCash, setShowCash] = useState(true)
+  const [showCredit, setShowCredit] = useState(false)
 
   const userId = localStorage.id
   useEffect(() => {
@@ -12,9 +16,20 @@ export default function MyBill() {
     })
       .then(res => {
         setBoughtHistory(res.data)
+        if (res.data.length) {
+          res.data.map(el => {
+            el.installment === false ? setBoughtHistoryCash(el) : setBoughtHistoryCredit(el)
+          })
+        }
       })
       .catch(err => {
         console.log(err.response);
+        Swal.fire({
+          title: 'Oops!',
+          text: err.response.data.message,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
       })
   }, [])
 
@@ -38,13 +53,17 @@ export default function MyBill() {
   }, []);
 
   const nextPayment = () => {
+    let CarId;
+    boughtHistory.map(el => {
+      CarId = el.CarId
+    })
+
     serverApi.post('/payments/credits/cars', {
-      CarId: 1
+      CarId
     }, {
       headers: {access_token: localStorage.access_token}
     })
     .then(res => {
-      console.log(res.data);
       window.snap.pay(res.data.token, {
         onSuccess: function(result){
           alert("payment success!"); console.log(result);
@@ -57,48 +76,158 @@ export default function MyBill() {
     })
   }
 
+  const changeShowCash = () => {
+    setShowCash(true)
+    setShowCredit(false)
+  }
+
+  const changeShowCredit = () => {
+    setShowCredit(true)
+    setShowCash(false)
+  }
+
 
   return (
     <>
-      <div className="flex justify-center bg-slate-100">
-        <div class="flex flex-col card bg-base-100 shadow-lg m-5 w-full mx-24">
+      <div className="flex flex-col justify-center w-full bg-slate-100">
+        <ul class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4 mx-24" id="tabs-tab"
+          role="tablist">
+          <li class="nav-item" role="presentation">
+            <button onClick={changeShowCash} class="
+              nav-link
+              block
+              font-medium
+              text-xs
+              leading-tight
+              uppercase
+              border-x-0 border-t-0 border-b-2 border-transparent
+              px-6
+              py-3
+              my-2
+              hover:border-transparent hover:bg-gray-100
+              focus:border-transparent
+              active
+            " data-bs-toggle="pill" data-bs-target="#tabs-home" role="tab" aria-controls="tabs-home"
+              aria-selected="true">Cash</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button onClick={changeShowCredit} className="
+              nav-link
+              block
+              font-medium
+              text-xs
+              leading-tight
+              uppercase
+              border-x-0 border-t-0 border-b-2 border-transparent
+              px-6
+              py-3
+              my-2
+              hover:border-transparent hover:bg-gray-100
+              focus:border-transparent
+            " id="tabs-profile-tab" data-bs-toggle="pill" data-bs-target="#tabs-profile" role="tab"
+              aria-controls="tabs-profile" aria-selected="false">Credit</button>
+          </li>
+          
+        </ul>
+        <div class="tab-content mx-24">
           {
-            boughtHistory.length ? boughtHistory.map(el => {
-              return (
-                <>
-                  <div class="card-body mx-20">
-                    <h2 className="card-title font-bold text-gray-800 mb-5">{el.carName}</h2>
-                    <div className="flex flex-row justify-between w-full">
-                      <span className="font-semibold text-slate-500">Submission Time</span>
-                      <span className="font-bold text-slate-700">{el.boughtDate}</span>
-                    </div>
-                    <div className="flex flex-row justify-between w-full">
-                      <span className="font-semibold text-slate-500">Total Installment</span>
-                      <span className="font-bold text-slate-700">{el.totalInstallment}</span>
-                    </div>
-                    <div className="flex flex-row justify-between w-full">
-                      <span className="font-semibold text-slate-500">Current Installment</span>
-                      <span className="font-bold text-slate-700">{el.currentInstallment}</span>
-                    </div>
-                    <div className="flex flex-row justify-between w-full">
-                      <span className="font-semibold text-slate-500">Status</span>
-                      {
-                        el.paidOff === true ? <span className="font-semibold bg-green-600 text-white rounded-md px-2">Already Paid</span> : <span className="font-semibold bg-red-600 text-white rounded-md px-2">Not Paid</span> 
-                      }
-                    </div>
-                    <div className="flex flex-row justify-between w-full">
-                      <span className="font-semibold text-slate-500">Order Id</span>
-                      <span className="font-bold text-slate-700">{el.orderId}</span>
+            showCash !== false ? 
+              <>
+                {
+                  boughtHistoryCash.installment === false ? 
+                  <div className="tab-pane fade show active" role="tabpanel" aria-labelledby="tabs-home-tab">
+                    <div class="flex flex-col card bg-base-100 shadow-lg m-5">
+                      <div class="card-body mx-20 ">
+                        <h2 className="card-title font-bold text-gray-800 mb-5">{boughtHistoryCash.carName}</h2>
+                        <div className="flex flex-row justify-between w-full">
+                          <span className="font-semibold text-slate-500">Submission Time</span>
+                          <span className="font-bold text-slate-700">{boughtHistoryCash.boughtDate}</span>
+                        </div>
+                        <div className="flex flex-row justify-between w-full">
+                          <span className="font-semibold text-slate-500">Total Price</span>
+                          <span className="font-bold text-slate-700">{boughtHistoryCash.price}</span>
+                        </div>
+                        <div className="flex flex-row justify-between w-full">
+                          <span className="font-semibold text-slate-500">Status</span>
+                          {
+                            boughtHistoryCash.paidOff === true ? <span className="font-semibold bg-green-600 text-white rounded-md px-2">Already Paid</span> : <span className="font-semibold bg-red-600 text-white rounded-md px-2">Not Paid</span> 
+                          }
+                        </div>
+                        <div className="flex flex-row justify-between w-full">
+                          <span className="font-semibold text-slate-500">Order Id</span>
+                          <span className="font-bold text-slate-700">{boughtHistoryCash.orderId}</span>
+                        </div>
+                      </div>
+                    </div> 
+                  </div>
+                  :
+                  <div className="tab-pane fade show active" role="tabpanel" aria-labelledby="tabs-home-tab">
+                    <div class="flex flex-col card bg-base-100 shadow-lg m-5 h-80">
+                      <div class="flex justify-center items-center card-body">
+                        <h1>You don't have a cash payment history yet</h1>
+                      </div>
                     </div>
                   </div>
-                  <hr />
-                </>
-              )
-            }): <div className="flex justify-center"><TailSpin  color="#00BFFF" height={80} width={80} /></div>
+                }
+              </>
+            :
+            <>
+              {
+                !boughtHistory.length ?
+                <div className="tab-pane fade show active" role="tabpanel" aria-labelledby="tabs-home-tab">
+                  <div class="flex flex-col card bg-base-100 shadow-lg m-5 h-80">
+                    <div class="flex justify-center items-center card-body">
+                      <h1>You don't have a credit payment history yet</h1>
+                    </div>
+                  </div>
+                </div>
+                :
+                boughtHistory.map(el => {
+                  return (
+                    el.installment === true ?
+                    <>
+                      <div class="tab-pane fade" id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
+                          <div class="flex card bg-base-100 shadow-lg m-5">
+                            <div class="card-body mx-20">
+                              <h2 className="card-title font-bold text-gray-800 mb-5">{el.carName}</h2>
+                              <div className="flex flex-row justify-between w-full">
+                                <span className="font-semibold text-slate-500">Submission Time</span>
+                                <span className="font-bold text-slate-700">{el.boughtDate}</span>
+                              </div>
+                              <div className="flex flex-row justify-between w-full">
+                                <span className="font-semibold text-slate-500">Total Installment</span>
+                                <span className="font-bold text-slate-700">{el.totalInstallment}</span>
+                              </div>
+                              <div className="flex flex-row justify-between w-full">
+                                <span className="font-semibold text-slate-500">Current Installment</span>
+                                <span className="font-bold text-slate-700">{el.currentInstallment}</span>
+                              </div>
+                              <div className="flex flex-row justify-between w-full">
+                                <span className="font-semibold text-slate-500">Status</span>
+                                {
+                                      el.paidOff === true ? <span className="font-semibold bg-green-600 text-white rounded-md px-2">Already Paid</span> : <span className="font-semibold bg-red-600 text-white rounded-md px-2">Not Paid</span> 
+                                    }
+                              </div>
+                              <div className="flex flex-row justify-between w-full">
+                                <span className="font-semibold text-slate-500">Order Id</span>
+                                <span className="font-bold text-slate-700">{el.orderId}</span>
+                              </div>
+                            </div>
+                            <hr />
+                          </div>
+                      </div>
+                    </>
+                    :  <div></div>
+                  
+                  )
+                }) 
+              }
+                <div className="flex justify-end m-8">
+                  <button onClick={() => nextPayment()} className="btn btn-sm">Next Payment</button>
+                </div>
+             
+            </>
           }
-          <div className="flex justify-end m-8">
-            <button onClick={() => nextPayment()} className="btn btn-sm">Next Payment</button>
-          </div>
         </div>
       </div>
     </>
